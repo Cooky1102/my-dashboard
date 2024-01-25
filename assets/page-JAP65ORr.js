5d4cd31928f34330a2dd270fd551031947d4cc90
cooky <437487609@qq.com>
2024-01-25 11:18:53 +0800

    Updates

---
+++ assets/page-JAP65ORr.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-KCc579kG.js";const t=()=>s.jsx("div",{className:"my-40 h-96 bg-indigo-50",children:"AnalyticsPage"});export{t as default};
--- assets/page-fYTXjOn5.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-lxRjVWda.js";const t=()=>s.jsx("div",{className:"my-40 h-96 bg-indigo-50",children:"AnalyticsPage"});export{t as default};
